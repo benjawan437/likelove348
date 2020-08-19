a2faf9e3d20eea64b5247d32dfdb11188cc15447
benjawn497 <benjawn497@gmail.com>
2020-08-19 21:06:28 +0700

    m

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{total}}
+    <div class="row">
+      <div class="col-3">
+        <counter lable="WAYV" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="LOVE" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="NCT" @cc="sum" />
+      </div>
+      <div class="col-3">
+        <counter lable="SM" @cc="sum" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import counter from "@/components/counter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,44 +1,43 @@
 <template>
-<div>
-    <div id="example-1">
-        <button @click="upFunction" class="btn btn-dark">{{lable}}</button>
-        <button @click="downFunction" class="btn btn-dark ml-3">-</button>
-        <p v-bind:style="{ fontSize: fsize + 'px' }">{{ counter }}</p>
+    <div>
+        <div id="example-1">
+            <button @click="upFunction" class="btn btn-info">{{ lable }}</button>
+            <button @click="downFunction" class="btn btn-info ml-3">{{ 0 }}</button>
+            <p v-bind:style="{ fontSize: fsize + 'px' }">HELLO {{counter}}</p>
+        </div>
     </div>
-</div>
 </template>
-
 <script>
 export default {
-    data: function () {
+    data: function() {
         return {
             counter: 0,
-            fsize: 50
+            fsize:20
         }
     },
     props: {
-        lable: String
+        lable: String,
     },
     methods: {
-        upFunction: function () {
+        upFunction: function() {
             this.counter += 1
             this.fsize += 10
-            console.log(this.fsize);
-            console.log(this.counter);
+
+            //console.log(this.fsize);
+            //console.log(this.counter);
+
+            //ส่งค่าตัวแปร counter ออกไปโดยผ่านตัวแปร cc
             this.$emit('cc', this.counter)
         },
-        downFunction: function () {
+        downFunction: function() {
             this.counter -= 1
             this.fsize -= 10
-            console.log(this.fsize);
-            console.log(this.counter);
-        }
-    }
+
+            //console.log(this.fsize);
+            //console.log(this.counter);
+
+            this.$emit('cc', this.counter)
+        },
+    },
 }
 </script>
-
-<style>
-    
-
-
-</style>
